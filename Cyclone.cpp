7c821cf13b52d5f514783f8602dc175c7fd1ddde
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-05-19 15:05:37 +0300

    Update Cyclone.cpp

--- Cyclone.cpp
+++ Cyclone.cpp
@@ -955,11 +955,11 @@ Int minKey, maxKey;
                                             Int matchingPrivateKey;
                                             matchingPrivateKey.Set(&currentBatchKey);
                                             int idx = pointIndices[j];
-                                            if (idx < 256) {
+                                            if (idx < 512) {
                                                 Int offset; offset.SetInt32(idx);
                                                 matchingPrivateKey.Add(&offset);
                                             } else {
-                                                Int offset; offset.SetInt32(idx - 256);
+                                                Int offset; offset.SetInt32(idx - 512);
                                                 matchingPrivateKey.Sub(&offset);
                                             }
                                             foundPrivateKeyHex = padHexTo64(intToHex(matchingPrivateKey));
